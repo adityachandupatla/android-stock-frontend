3bf22b3ce22c103e76ff1e6792520c9af7dc0cd0
Aditya Ch <ch.aditya1996@gmail.com>
2020-11-26 14:39:15 -0800

    Design app toolbar

--- app/src/main/java/com/csci571/aditya/stockapp/MainActivity.java
+++ app/src/main/java/com/csci571/aditya/stockapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.csci571.aditya.stockapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -11,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
         setTheme(R.style.Theme_StockApplication);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+
+        setSupportActionBar(myToolbar);
     }
 }
