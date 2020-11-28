033125ae52922d9e525f7e9d4adf90acbf3858b8
Aditya Ch <ch.aditya1996@gmail.com>
2020-11-27 19:47:36 -0800

    Home screen done - Volley, Swipe, Drag-drop

--- app/src/main/java/com/csci571/aditya/stockapp/MainActivity.java
+++ app/src/main/java/com/csci571/aditya/stockapp/MainActivity.java
@@ -4,13 +4,17 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import io.github.luizgrp.sectionedrecyclerviewadapter.SectionedRecyclerViewAdapter;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.csci571.aditya.stockapp.favorite.Favorite;
 import com.csci571.aditya.stockapp.favorite.FavoriteSection;
@@ -20,18 +24,21 @@ import com.csci571.aditya.stockapp.info.SectionItemInfoFactory;
 import com.csci571.aditya.stockapp.localstorage.AppStorage;
 import com.csci571.aditya.stockapp.localstorage.FavoriteStorageModel;
 import com.csci571.aditya.stockapp.localstorage.PortfolioStorageModel;
+import com.csci571.aditya.stockapp.network.StockAppClient;
 import com.csci571.aditya.stockapp.portfolio.Portfolio;
 import com.csci571.aditya.stockapp.portfolio.PortfolioSection;
+import com.csci571.aditya.stockapp.utils.Constants;
+import com.csci571.aditya.stockapp.utils.Parser;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 
 
-
 public class MainActivity extends AppCompatActivity implements PortfolioSection.ClickListener, FavoriteSection.ClickListener {
 
+    private RecyclerView recyclerView;
     private SectionedRecyclerViewAdapter sectionAdapter;
 
     private static final String TAG = "com.csci571.aditya.stockapp.MainActivity";
@@ -46,44 +53,56 @@ public class MainActivity extends AppCompatActivity implements PortfolioSection.
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
+        double uninvestedCash = AppStorage.getUninvestedCash(getApplicationContext());
         ArrayList<PortfolioStorageModel> portfolioStorageModels = AppStorage.getPortfolio(getApplicationContext());
         ArrayList<FavoriteStorageModel> favoriteStorageModels = AppStorage.getFavorites(getApplicationContext());
-
-        Log.i(TAG, "Portfolio details:");
-        for (PortfolioStorageModel portfolioStorageModel: portfolioStorageModels) {
-            Log.i(TAG, portfolioStorageModel.toString());
-        }
-
-        Log.i(TAG, "Favorite details:");
-        for (FavoriteStorageModel favoriteStorageModel: favoriteStorageModels) {
-            Log.i(TAG, favoriteStorageModel.toString());
-        }
-
         HashSet<String> tickerSet = uniqueTickers(portfolioStorageModels, favoriteStorageModels);
-        for (String ticker: tickerSet) {
-            Log.i(TAG, "ticker: " + ticker);
-        }
 
         sectionAdapter = new SectionedRecyclerViewAdapter();
 
         List<Portfolio> portfolioList = new ArrayList<>();
-        portfolioList.add(new Portfolio("MSFT", 8.0, 202.68, 10.57));
-        portfolioList.add(new Portfolio("MSFT", 8.0, 202.68, 10.57));
+        for (PortfolioStorageModel portfolioStorageModel: portfolioStorageModels) {
+            portfolioList.add(new Portfolio(portfolioStorageModel.getStockTicker(),
+                    portfolioStorageModel.getSharesOwned(), 0, 0,
+                    portfolioStorageModel.getTotalAmount()));
+        }
+        sectionAdapter.addSection(Constants.PORTFOLIO_SECTION_TAG,
+                new PortfolioSection(Parser.beautify(uninvestedCash), portfolioList, this));
 
         List<Favorite> favList = new ArrayList<>();
-        favList.add(new Favorite("MSFT", 8.0, "Microsoft Corp.", 202.68, 10.57));
-        favList.add(new Favorite("MSFT", 8.0, "Microsoft Corp.", 202.68, 10.57));
+        for (FavoriteStorageModel favoriteStorageModel: favoriteStorageModels) {
+            double shares = getSharesOfFavoriteStock(favoriteStorageModel.getStockTicker(), portfolioStorageModels);
+            favList.add(new Favorite(favoriteStorageModel.getStockTicker(), shares, favoriteStorageModel.getCompanyName(),
+                    0, 0, favoriteStorageModel.getLastPrice()));
+        }
+        sectionAdapter.addSection(Constants.FAVORITE_SECTION_TAG,
+                new FavoriteSection(favList, this));
 
-        sectionAdapter.addSection(new PortfolioSection("19450.26", portfolioList, this));
-        sectionAdapter.addSection(new FavoriteSection(favList, this));
-
-        // Set up your RecyclerView with the SectionedRecyclerViewAdapter
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.main_recycler_view);
+        recyclerView = findViewById(R.id.main_recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
         recyclerView.setAdapter(sectionAdapter);
-
         DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), LinearLayout.VERTICAL);
         recyclerView.addItemDecoration(dividerItemDecoration);
+
+        enableSwipeToDelete();
+
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+
+        recyclerView.setVisibility(View.INVISIBLE);
+        progressBar.setVisibility(View.VISIBLE);
+
+        StockAppClient.getInstance(getApplicationContext()).fetchHomeScreenData(tickerSet,
+                progressBar, recyclerView, sectionAdapter);
+
+    }
+
+    private double getSharesOfFavoriteStock(String stockTicker, ArrayList<PortfolioStorageModel> portfolioStorageModels) {
+        for (PortfolioStorageModel portfolioStorageModel: portfolioStorageModels) {
+            if (portfolioStorageModel.getStockTicker().equals(stockTicker)) {
+                return portfolioStorageModel.getSharesOwned();
+            }
+        }
+        return 0;
     }
 
     private HashSet<String> uniqueTickers(ArrayList<PortfolioStorageModel> portfolioStorageModels, ArrayList<FavoriteStorageModel> favoriteStorageModels) {
@@ -113,5 +132,83 @@ public class MainActivity extends AppCompatActivity implements PortfolioSection.
                 SectionInfoFactory.create(section, sectionAdapter.getAdapterForSection(section))
         );
         dialog.show(getSupportFragmentManager(), "EFGH");
+    }
+
+    private void enableSwipeToDelete() {
+        SwipeToDeleteCallback swipeToDeleteCallback = new SwipeToDeleteCallback(this) {
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+                final int position = viewHolder.getAdapterPosition();
+                String ticker = ((TextView) viewHolder.itemView.findViewById(R.id.ticker_fav)).getText().toString();
+                FavoriteSection favoriteSection = (FavoriteSection) sectionAdapter.getSection(Constants.FAVORITE_SECTION_TAG);
+                List<Favorite> favoriteList = favoriteSection.getList();
+                for (Favorite favorite: favoriteList) {
+                    if (favorite.getTicker().equals(ticker)) {
+                        favoriteList.remove(favorite);
+                        break;
+                    }
+                }
+                AppStorage.removeFromFavorite(getApplicationContext(), ticker);
+                sectionAdapter.notifyItemRemoved(position);
+            }
+
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder,
+                                  @NonNull RecyclerView.ViewHolder targetHolder) {
+                int fromPosition = viewHolder.getAdapterPosition();
+                int toPosition = targetHolder.getAdapterPosition();
+
+                PortfolioSection portfolioSection = (PortfolioSection) sectionAdapter.getSection(Constants.PORTFOLIO_SECTION_TAG);
+                List<Portfolio> portfolioList = portfolioSection.getList();
+
+                FavoriteSection favoriteSection = (FavoriteSection) sectionAdapter.getSection(Constants.FAVORITE_SECTION_TAG);
+                List<Favorite> favoriteList = favoriteSection.getList();
+
+                if (viewHolder.itemView.findViewById(R.id.ticker) != null &&
+                        targetHolder.itemView.findViewById(R.id.ticker) != null) {
+                    Log.i(TAG, "from: " + fromPosition + " to: " + toPosition + " size: " + portfolioList.size());
+                    if (fromPosition < toPosition) {
+                        for (int i = fromPosition - 1; i < toPosition - 1; i++) {
+                            Collections.swap(portfolioList, i, i + 1);
+                        }
+                    } else {
+                        for (int i = fromPosition - 1; i > toPosition - 1; i--) {
+                            Collections.swap(portfolioList, i, i - 1);
+                        }
+                    }
+                    AppStorage.updatePortfolioOrder(getApplicationContext(), portfolioList);
+                    sectionAdapter.notifyItemMoved(fromPosition, toPosition);
+                    Log.i(TAG, "New Size: " + portfolioList.size());
+                    return super.onMove(recyclerView, viewHolder, targetHolder);
+                }
+                if (viewHolder.itemView.findViewById(R.id.ticker_fav) != null &&
+                        targetHolder.itemView.findViewById(R.id.ticker_fav) != null) {
+                    // ensure the offsets are correctly established before accessing the list
+                    // this is done because favorite list is below portfolio list
+                    int offset = portfolioList.size() + 1;
+                    fromPosition -= offset;
+                    toPosition -= offset;
+                    Log.i(TAG, "from: " + fromPosition + " to: " + toPosition + " size: " + favoriteList.size());
+                    if (fromPosition < toPosition) {
+                        for (int i = fromPosition - 1; i < toPosition - 1; i++) {
+                            Collections.swap(favoriteList, i, i + 1);
+                        }
+                    } else {
+                        for (int i = fromPosition - 1; i > toPosition - 1; i--) {
+                            Collections.swap(favoriteList, i, i - 1);
+                        }
+                    }
+                    AppStorage.updateFavoriteOrder(getApplicationContext(), favoriteList);
+                    // re-add the offsets
+                    sectionAdapter.notifyItemMoved(fromPosition + offset, toPosition + offset);
+                    Log.i(TAG, "New Size: " + favoriteList.size());
+                    return super.onMove(recyclerView, viewHolder, targetHolder);
+                }
+                return false;
+            }
+        };
+
+        ItemTouchHelper itemTouchhelper = new ItemTouchHelper(swipeToDeleteCallback);
+        itemTouchhelper.attachToRecyclerView(recyclerView);
     }
 }
--- app/src/main/java/com/csci571/aditya/stockapp/utils/Constants.java
+++ app/src/main/java/com/csci571/aditya/stockapp/utils/Constants.java
@@ -1,9 +1,42 @@
 package com.csci571.aditya.stockapp.utils;
 
+import com.csci571.aditya.stockapp.localstorage.FavoriteStorageModel;
+import com.csci571.aditya.stockapp.localstorage.PortfolioStorageModel;
+
 public class Constants {
+    // Constants related to shared preferences
     public static final String PORTFOLIO_PREF_NAME = "portfolio";
     public static final String PORTFOLIO_KEY = "MyPortfolio";
     public static final String FAVORITES_PREF_NAME = "favorites";
     public static final String FAVORITES_KEY = "MyFavorites";
     public static final String APP_FIRST_TIME = "firstTime";
+    public static final String UNINVESTED_PREF_NAME = "uninvested";
+    public static final String UNINVESTED_KEY = "MyUninvestedCash";
+
+    // Constants related to network calls
+    public static final String DEVELOPMENT_HOST = "http://10.0.2.2:8080";
+    public static final String PROD_HOST = "https://android-node-backend.wl.r.appspot.com";
+    public static final String OUTLOOK_ENDPOINT_TEMPLATE = "/stock/api/v1.0/outlook/%s";
+    public static final String SUMMARY_ENDPOINT_TEMPLATE = "/stock/api/v1.0/summary/%s";
+    public static final String HISTORICAL_ENDPOINT_TEMPLATE = "/stock/api/v1.0/historical/%s?startDate=%s";
+    public static final String DAILY_ENDPOINT_TEMPLATE = "/stock/api/v1.0/daily/%s?startDate=%s&resampleFreq=4min";
+    public static final String AUTOCOMPLETE_ENDPOINT_TEMPLATE = "/stock/api/v1.0/search?query=%s";
+    public static final String NEWS_ENDPOINT_TEMPLATE = "/stock/api/v1.0/news/%s?q=%s";
+
+    // Default portfolio
+    public static PortfolioStorageModel MICROSOFT_PORTFOLIO = new PortfolioStorageModel("MSFT", 8.0, 1619.76);
+    public static PortfolioStorageModel GOOGLE_PORTFOLIO = new PortfolioStorageModel("GOOGL", 5.0, 8080.55);
+    public static PortfolioStorageModel APPLE_PORTFOLIO = new PortfolioStorageModel("AAPL", 5.0, 544.3);
+    public static PortfolioStorageModel TESLA_PORTFOLIO = new PortfolioStorageModel("TSLA", 3.0, 1164.12);
+
+    // Default favorites
+    public static FavoriteStorageModel NETFLIX_FAVORITE = new FavoriteStorageModel("NFLX", "NetFlix Inc", 491.36);
+    public static FavoriteStorageModel GOOGLE_FAVORITE = new FavoriteStorageModel("GOOGL", "Alphabet Inc - Class A", 1787.02);
+    public static FavoriteStorageModel APPLE_FAVORITE = new FavoriteStorageModel("AAPL", "Apple Inc", 116.59);
+    public static FavoriteStorageModel TESLA_FAVORITE = new FavoriteStorageModel("TSLA", "Tesla Inc", 585.76);
+
+    // Misc
+    public static final String PORTFOLIO_SECTION_TAG = "PortfolioTag";
+    public static final String FAVORITE_SECTION_TAG = "FavoriteTag";
+
 }
