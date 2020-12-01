c0e4c59ef1d08e070fac6faed2c99569a2b40e4c
Aditya Ch <ch.aditya1996@gmail.com>
2020-12-01 14:25:54 -0800

    Use DecimalFormat, and null checks in getTimeAgo

--- app/src/main/java/com/csci571/aditya/stockapp/utils/Parser.java
+++ app/src/main/java/com/csci571/aditya/stockapp/utils/Parser.java
@@ -6,36 +6,44 @@ import com.csci571.aditya.stockapp.models.ArticleModel;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.text.DecimalFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class Parser {
 
     private static final String TAG = "com.csci571.aditya.stockapp.utils.Parser";
 
     public static String beautify(double val) {
-        // TODO: Need to round off up to two decimal places (not EXACTLY two)
-        double roundOff = Math.round(val * 100.0) / 100.0;
-        return String.valueOf(roundOff);
+        DecimalFormat formatter = new DecimalFormat("#,##0.00");
+        return formatter.format(val);
     }
 
     public static String urlEncode(String s) {
         try {
             return URLEncoder.encode(s, "UTF-8");
         } catch (UnsupportedEncodingException e) {
-            Log.i(TAG, "Unable to URL encode the string: " + s + " UTF-8 should always be supported", e);
+            Log.e(TAG, "Unable to URL encode the string: " + s + " UTF-8 should always be supported", e);
             return "";
         }
     }
 
     public static String getTimeAgo(ArticleModel articleModel, int position) {
-        SimpleDateFormat input = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        if (articleModel == null || articleModel.getDate() == null || articleModel.getTitle() == null) {
+            Log.e(TAG, "Cannot create timeAgo string with null values in: " + ArticleModel.class.getName());
+        }
+        SimpleDateFormat input = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US);
         String timeAgo = "";
         try {
             Date publishedDate = input.parse(articleModel.getDate());
             Date today = new Date();
             long diffInMilliseconds = today.getTime() - publishedDate.getTime();
+            if (diffInMilliseconds < 0) {
+                Log.e(TAG, "Published date of article: " + articleModel.getTitle() + " is greater than current date!");
+                return timeAgo;
+            }
             if (diffInMilliseconds < 3600000) {
                 int mins = (int) (diffInMilliseconds / 60000);
                 timeAgo = mins + " mins ago";
@@ -49,7 +57,7 @@ public class Parser {
                 timeAgo = days + " days ago";
             }
         } catch (ParseException e) {
-            Log.i(TAG, "Unable to parse date for article at position: "
+            Log.e(TAG, "Unable to parse date for article at position: "
                     + position + " with title: " + articleModel.getTitle());
         }
         return timeAgo;
